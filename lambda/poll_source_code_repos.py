72cb1572f9a909ca1f2ebeb494f66cbce383b64f
Renat <rzhilkibaev@gmail.com>
2016-07-31 11:42:48 -0700

    Follow directory layout

--- lambda/poll_source_code_repos.py
+++
@@ -1,7 +0,0 @@
-from __future__ import print_function
-
-import json
-
-def lambda_handler(event, context):
-    print("Received event: " + json.dumps(event, indent=2))
-    return { "ping": "pong"}
