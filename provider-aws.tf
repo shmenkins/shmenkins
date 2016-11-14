99cbf328824b2b9f94e62491ef78d16480c8bd21
Renat <rzhilkibaev@gmail.com>
2016-11-13 22:26:37 -0800

    Remove old code

--- provider-aws.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_credentials" {}
-variable "aws_account_id" {}
-variable "aws_region" {}
-
-provider "aws" {
-    shared_credentials_file  = "${var.aws_credentials}"
-    region = "${var.aws_region}"
-}
