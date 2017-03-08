07cc492e8e8677edd22a5bd6f9bd6b32ba5afdde
Renat <rzhilkibaev@gmail.com>
2017-03-08 13:44:45 -0800

    Cleanup

--- lambda/builder/src/main/java/com/shmenkins/core/handler/builder/Builder.java
+++ lambda/builder/src/main/java/com/shmenkins/core/handler/builder/Builder.java
@@ -1,14 +1,7 @@
 package com.shmenkins.core.handler.builder;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
@@ -52,17 +45,15 @@ public class Builder implements BasicHandler<BuildScheduledEvent, Void> {
             String destDir = "/tmp";
             log.debug("Extracting source code; file={}", zipFile);
             new ZipFile(zipFile).extractAll(destDir);
-//            FileUtils.deleteQuietly(zipFile);
-            // unzip(zipFile, destDir);
+            FileUtils.deleteQuietly(zipFile);
 
             // download build tool
             log.debug("Getting maven source code; file={}", zipFile);
             File mavenZip = binStorage.getFile("apache-maven-3.3.9-bin.zip", "/tmp/apache-maven-3.3.9-bin.zip");
             log.debug("extracting maven source code; file={}", mavenZip);
-            new ZipFile(mavenZip).extractAll("/tmp");
-//            FileUtils.deleteQuietly(mavenZip);
-
+            FileUtils.deleteQuietly(mavenZip);
             // extract
+            new ZipFile(mavenZip).extractAll("/tmp");
 
             // build
         } catch (Exception e) {
@@ -70,52 +61,6 @@ public class Builder implements BasicHandler<BuildScheduledEvent, Void> {
         }
 
         return null;
-    }
-
-    private void unzip(String zip, String destDir) {
-        try {
-            byte[] buffer = new byte[1024];
-            ZipInputStream zis = new ZipInputStream(new FileInputStream(zip));
-            try {
-                ZipEntry zipEntry;
-                while ((zipEntry = zis.getNextEntry()) != null) {
-                    String fileName = zipEntry.getName();
-                    log.debug("Extracting {}", fileName);
-                    File newFile = new File(destDir + "/" + fileName);
-                    if (zipEntry.isDirectory()) {
-                        newFile.mkdirs();
-                    } else {
-                        File parent = newFile.getParentFile();
-                        if (parent != null) {
-                            parent.mkdirs();
-                        }
-                        try (FileOutputStream fos = new FileOutputStream(newFile)) {
-                            int len;
-                            while ((len = zis.read(buffer)) > 0) {
-                                fos.write(buffer, 0, len);
-                            }
-                        }
-                    }
-                }
-            } finally {
-                zis.closeEntry();
-                zis.close();
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Cannot unpack zip; zipFile=" + zip, e);
-        }
-    }
-
-    private void downloadFile(String url, String dest) {
-        try (InputStream is = new URL(url).openStream()) {
-            try (ReadableByteChannel rbc = Channels.newChannel(is)) {
-                try (FileOutputStream fos = new FileOutputStream(dest)) {
-                    fos.getChannel().transferFrom(rbc, 0, Long.MAX_VALUE);
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException("Cannot download a file; url=" + url, e);
-        }
     }
 
     public static void main(String[] args) {
