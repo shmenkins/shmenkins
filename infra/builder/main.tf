3491eb0393fcd80caa6458d63c4e07aba9e7f8f2
Renat <rzhilkibaev@gmail.com>
2017-04-08 21:01:08 -0700

    Add SNS as event source

--- infra/builder/main.tf
+++ infra/builder/main.tf
@@ -1,5 +1,11 @@
 variable "s3_bucket" {}
 
+# topic that the lambda listens to
+resource "aws_sns_topic" "build_scheduled" {
+  name = "build_scheduled"
+}
+
+# builder lambda
 module "builder_lambda" {
   source = "github.com/rzhilkibaev/logging_lambda.tf"
   name = "builder"
@@ -7,3 +13,8 @@ module "builder_lambda" {
   s3_key = "artifacts/builder.zip"
 }
 
+module "lambda_event_source_sns" {
+  source = "github.com/rzhilkibaev/lambda_event_source_sns.tf"
+  topic_arn = "${aws_sns_topic.build_scheduled.arn}"
+  function_arn = "${module.builder_lambda.function_arn}"
+}
