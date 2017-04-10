f311afcffaed10098275d77f6466c4a788456a5f
Renat <rzhilkibaev@gmail.com>
2017-04-10 00:04:51 -0700

    [builder] Publish dummy build status

--- lambda-py/builder/main.py
+++ lambda-py/builder/main.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 import logging
 import os
 
@@ -13,13 +14,28 @@ logger.setLevel(log_level)
 
 sns = boto3.client("sns")
 
-def handler(event, context):
 
+def handler(event, context):
     logger.debug("Handling %s", str(event))
 
-    message = "finished"
-    sns.publish(TopicArn=topic_arn_build_status_change,
-            Message=message)
+    build_scheduled_event = parse_build_scheduled_event(event)
+
+    publish_status_change_event(build_scheduled_event["url"], "finished")
 
     logger.debug("Finished handling %s", str(event))
 
+
+def parse_build_scheduled_event(event):
+    return json.loads(event["Records"][0]["Sns"]["Message"])
+
+
+def publish_status_change_event(url, status):
+    build_status_change_event = {
+        "url": url,
+        "status": status
+    }
+    publish_event(topic_arn_build_status_change, build_status_change_event)
+
+
+def publish_event(topic_arn, event):
+    sns.publish(TopicArn=topic_arn, Message=json.dumps(event))
--- lambda-py/builder/tests/test_handler.py
+++ lambda-py/builder/tests/test_handler.py
@@ -1,4 +1,35 @@
-from shmenkins.builder import builder
+import os
+import boto3
+from mock import MagicMock
 
-def test_builder():
-    pass
+# prepare environment before importing main
+region = boto3.session.Session().region_name
+account = boto3.client("sts").get_caller_identity().get("Account")
+os.environ["TOPIC_ARN_BUILD_STATUS_CHANGE"] = "arn:aws:sns:" + region + ":" + account + ":build_status_change"
+
+import main
+
+event = {u'Records': [{u'EventVersion': u'1.0',
+                       u'EventSubscriptionArn': u'arn:aws:sns:us-west-2:000000000000:build_scheduled:00000000-0000-0000-0000-000000000000',
+                       u'EventSource': u'aws:sns',
+                       u'Sns': {u'SignatureVersion': u'1', u'Timestamp': u'2017-04-10T05:12:56.297Z',
+                                u'Signature': u'xxx',
+                                u'SigningCertUrl': u'xxx',
+                                u'MessageId': u'0000000000000-0000-0000-000000000000', u'Message': u'{"url": "https://github.com/foo/bar"}',
+                                u'MessageAttributes': {
+                                    u'AWS.SNS.MOBILE.MPNS.NotificationClass': {u'Type': u'String',
+                                                                               u'Value': u'realtime'},
+                                    u'AWS.SNS.MOBILE.WNS.Type': {u'Type': u'String', u'Value': u'wns/badge'},
+                                    u'AWS.SNS.MOBILE.MPNS.Type': {u'Type': u'String', u'Value': u'token'}},
+                                u'Type': u'Notification',
+                                u'UnsubscribeUrl': u'xxx',
+                                u'TopicArn': u'arn:aws:sns:us-west-2:000000000000:build_scheduled', u'Subject': None}}]}
+
+
+def test_build_status_published():
+    main.publish_event = MagicMock()
+    main.handler(event, None)
+    expected_topic_arn = "arn:aws:sns:" + region + ":" + account + ":build_status_change"
+    expected_event = {"url": "https://github.com/foo/bar", "status": "finished"}
+    main.publish_event.assert_called_with(expected_topic_arn, expected_event)
+
