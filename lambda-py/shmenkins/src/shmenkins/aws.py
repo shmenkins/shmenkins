cc1201a3f30bcb6e7e926e4b0ce7080e63d7e7e7
Renat <rzhilkibaev@gmail.com>
2017-07-17 20:55:26 -0700

    [shmenkins] add a way to get aws account

--- lambda-py/shmenkins/src/shmenkins/aws.py
+++ lambda-py/shmenkins/src/shmenkins/aws.py
@@ -2,16 +2,6 @@ import boto3
 import json
 
 
-class Aws:
-    def __init__(self) -> None:
-        self.__region = boto3.session.Session().region_name
-        self.__account = boto3.client("sts").get_caller_identity().get("Account")
-        self.__sns = boto3.resource("sns")
-
-    def get_topic(self, topic_name: str) -> Topic:
-        return Topic(self.__sns.Topic("arn:aws:sns:" + self.__region + ":" + self.__account + ":" + topic_name))
-
-
 class Topic:
     def __init__(self, topic) -> None:
         self.__topic = topic
@@ -19,3 +9,16 @@ class Topic:
     def publish(self, obj: dict) -> None:
         """ Publish a message, obj must be dict and is converted to json """
         self.__topic.publish(Message=json.dumps(obj))
+
+
+class Aws:
+    def __init__(self) -> None:
+        self.__region = boto3.session.Session().region_name  # type: str
+        self.__account = boto3.client("sts").get_caller_identity().get("Account")  # type: str
+        self.__sns = boto3.resource("sns")
+
+    def get_account(self) -> str:
+        return self.__account
+
+    def get_topic(self, topic_name: str) -> Topic:
+        return Topic(self.__sns.Topic("arn:aws:sns:" + self.__region + ":" + self.__account + ":" + topic_name))
