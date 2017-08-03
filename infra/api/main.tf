a9fbafaaf1b6aef6013fff3344d280d640d8c22b
Renat <rzhilkibaev@gmail.com>
2017-08-02 22:39:45 -0700

    [api] add swagger import

--- infra/api/main.tf
+++ infra/api/main.tf
@@ -1,5 +1,27 @@
 variable "s3_bucket" {}
 
+resource "aws_api_gateway_rest_api" "shmenkins" {
+  name = "shmenkins"
+}
+
+resource "null_resource" "swagger" {
+  triggers {
+    swagger_file_hash = "${sha1(file("api.yaml"))}"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      aws apigateway put-rest-api \
+        --profile=shmenkins \
+        --region=${data.aws_region.current.name} \
+        --mode=overwrite \
+        --rest-api-id=${aws_api_gateway_rest_api.shmenkins.id} \
+        --body='file://api.yaml'
+EOF
+  }
+
+  depends_on = ["aws_api_gateway_rest_api.shmenkins"]
+}
 
 data "aws_region" "current" {
   current = true
